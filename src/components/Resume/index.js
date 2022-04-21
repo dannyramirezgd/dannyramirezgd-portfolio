fb836803a0ffe095b0f87a106c129deeccfbb74d
dannyrgd <danny.ramirezgd@gmail.com>
2022-04-21 18:39:35 -0400

    added css changes to resume sections

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -13,70 +13,70 @@ function Resume() {
       </h1>
       <h2 className='mx-4'>Proficiencies</h2>
       <ListGroup className='d-flex flex-row flex-wrap justify-content-between align-items-center'>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require(`../../assets/images/jsIcon.png`)}
             alt="javascript icon"
           />
           <p className="text-center col-12">Javascript</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/bootstrapIcon.png')}
             alt="bootstrap icon"
           />
           <p className="text-center col-12">Bootstrap</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/cssIcon.png')}
             alt="css icon"
           />
           <p className="text-center col-12">CSS</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/mysqlIcon.png')}
             alt="MySQL icon"
           />
           <p className="text-center col-12">MySQL</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/nosqlIcon.png')}
             alt="noSQL icon"
           />
           <p className="text-center col-12">noSQL</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/nodejsIcon.png')}
             alt="Node.JS icon"
           />
           <p className="text-center col-12">Node.js</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/mongodbIcon.png')}
             alt="mongodb icon"
           />
           <p className="text-center col-12">MongoDB</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/restapiIcon.png')}
             alt="RestAPIs icon"
           />
           <p className="text-center col-12">Rest APIs</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/reactLogo.png')}
             alt="RestAPIs icon"
           />
           <p className="text-center col-12">React</p>
         </ListGroup.Item>
-        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap'>
+        <ListGroup.Item className='imgs m-auto d-flex align-items-center justify-content-around flex-wrap rounded mt-2'>
           <img
             src={require('../../assets/images/graphqlIcon.png')}
             alt="RestAPIs icon"
